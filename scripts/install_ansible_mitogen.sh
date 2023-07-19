54d8a96da75a746ec966c0886b8344bb6556b517
NAME <mail@gmail.com>
2023-07-19 07:39:21 +0530

    backup

--- scripts/install_ansible_mitogen.sh
+++ scripts/install_ansible_mitogen.sh
@@ -1,9 +1,19 @@
 set -x
 
+# mitogen is not improving anything in connection: local or even localhost ssh (getting exact same exec times)
+
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 apt install python python-pip
 pip install -U pipx
 pip install mitogen
 
-pipx install ansible --system-site-packages --include-deps
+strategy_dir=$(python -c "import ansible_mitogen; from pathlib import Path; print(Path(ansible_mitogen.__file__).parent / 'plugins/strategy')")
+mkdir -p $PREFIX/etc/ansible
+cat > $PREFIX/etc/ansible/ansible.cfg <<EOF
+[defaults]
+strategy_plugins = $strategy_dir
+EOF
+
+# export ANSIBLE_CONFIG=$PREFIX/etc/ansible/ansible.cfg
+pipx install ansible==6.7.0 --system-site-packages --include-deps --verbose --force
--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,3 +1,5 @@
 set -x
 
+# TODO patch doesn't work with ansible 6.7.0 at all and requires a shell script
+
 ANSIBLE_STDOUT_CALLBACK=yaml ./playbooks/termux_patch.yml -v
